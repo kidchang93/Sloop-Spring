d4d984e905e6f48064176631af7afe7ea62e8beb
kidchang93 <lck930721@gmail.com>
2024-01-06 18:55:56 +0900

    docs : 회원정보 수정시 체크박스/라디오 박스 데이터 불러오기 완료

--- src/main/java/kr/co/sloop/member/controller/MemberController.java
+++ src/main/java/kr/co/sloop/member/controller/MemberController.java
@@ -23,7 +23,7 @@ public class MemberController {
     // signupForm.jsp 로 이동
     @GetMapping("/signup")
     public String signupForm(){
-        return "signupForm";
+        return "member/signupForm";
     }
 
     // signupForm.jsp -> form method = Post 로 데이터 받아옴
@@ -32,16 +32,16 @@ public class MemberController {
         int signupResult = memberService.signup(memberDTO);
 
         if (signupResult > 0){
-            return "signupSuccess"; // 회원가입 성공 시 이동
+            return "member/signupSuccess"; // 회원가입 성공 시 이동
         } else {
-            return "signupForm";    // 회원가입 실패 시 이동
+            return "member/signupForm";    // 회원가입 실패 시 이동
         }
     }
 
     // 회원가입 성공 후 로그인 하러 가기 버튼 클릭시 LoginForm.jsp 로 이동
     @GetMapping("/login")
     public String loginForm(){
-        return "loginForm";
+        return "member/loginForm";
     }
 
 
@@ -57,7 +57,7 @@ public class MemberController {
             return "home"; // 로그인 성공시 세션에 "loginEmail"이란 이름으로 저장 후 studyList or mypage 로 이동
         } else {
             log.info("로그인 실패");
-            return "redirect:/member/login";    // 로그인 실패시 다시 GetMapping 의 LoginForm으로 redirect
+            return "redirect:member/member/login";    // 로그인 실패시 다시 GetMapping 의 LoginForm으로 redirect
         }
     }
 
@@ -80,7 +80,7 @@ public class MemberController {
     public String memberList(Model model){
         List<MemberDTO> memberDTOList = memberService.findMemberList(model);
         model.addAttribute("memberList", memberDTOList);
-        return "memberList";    // 관리자 일 때 memberList 이동 가능
+        return "member/memberList";    // 관리자 일 때 memberList 이동 가능
     }
 
     // update.jsp의 Form 출력
@@ -91,9 +91,9 @@ public class MemberController {
         if (loginEmail != null) {
             MemberDTO memberDTO = memberService.findByMemberEmail(loginEmail);
             model.addAttribute("member", memberDTO);
-            return "update";
+            return "member/update";
         } else {
-            return "redirect:/member/login";
+            return "redirect:/member/member/login";
         }
 
 
@@ -105,9 +105,9 @@ public class MemberController {
     public String update (@ModelAttribute MemberDTO memberDTO){
         boolean result = memberService.update(memberDTO);
         if (result) {
-            return "redirect:/member?memberIdx=" + memberDTO.getMemberIdx();    // update 성공시 redirect로 상세보기 화면 출력
+            return "redirect:/member/member?memberIdx=" + memberDTO.getMemberIdx();    // update 성공시 redirect로 상세보기 화면 출력
         } else {
-            return "redirect:/member/update";  // update 실패시 다시 수정할 수 있게 update.jsp로 정보 가져가면서 redirect 어케함?
+            return "redirect:/member/member/update";  // update 실패시 다시 수정할 수 있게 update.jsp로 정보 가져가면서 redirect 어케함?
         }
     }
 
@@ -116,7 +116,7 @@ public class MemberController {
     public String findByIdx(@RequestParam("memberIdx") int memberIdx , Model model){
         MemberDTO memberDTO = memberService.findByIdx(memberIdx);   // memberIdx 파라미터 값을 가져온 뒤 해당 domain 정보를 불러온다.
         model.addAttribute("member",memberDTO);
-        return "mypage";
+        return "member/mypage";
     }
 
     // 꼭 로그인 후 마이페이지로 이동 ( 회원의 기능 )
@@ -133,7 +133,7 @@ public class MemberController {
 
             return "redirect:/member?memberIdx="+memberDTO.getMemberIdx();  // 세션에 저장된 아이디에 맞는 마이페이지로 이동
         } else{
-            return "loginForm"; // 세션에 있는 아이디가 없거나 맞지 않으면 loginForm으로 이동
+            return "member/loginForm"; // 세션에 있는 아이디가 없거나 맞지 않으면 loginForm으로 이동
         }
 
     }
@@ -142,10 +142,10 @@ public class MemberController {
     @GetMapping("/logout")
     public String logout(HttpSession session){
         session.invalidate();
-        return "logout";    // 로그아웃 페이지로 이동
+        return "member/logout";    // 로그아웃 페이지로 이동
     }
 
-    // 회원 탈퇴 시
+    // 리스트에서 회원 삭제시 & 회원 탈퇴 시
     @GetMapping("/delete")
     public String deleteByUser(@RequestParam ("memberIdx") int memberIdx){
         int deleteResult = memberService.deleteByUser(memberIdx);
